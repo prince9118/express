e84239258eaa53441f5b5fd637096b530906b589
Prince Chaudhary <pchaudharyskn@gmail.com>
2025-11-09 22:07:33 +0530

    express api

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-for (let i = 0; i < 3; i++) {
-  setTimeout(function() {
-    console.log(i);
-  }, 100);
-  // console.log(i);
-}
+const express= require('express');
+const app=express();
+
+
+app.get('/',(req,res)=>{
+res.send("Welcome to the home page!");
+});
+app.get('/health-checkup',(req,res)=>{
+    res.send("your kidney is healthy");
+
+});
+// global catches
+
+app.use(function(err,req,res,next ){
+    res.json("something is not going right in the website");
+});
+app.listen(3000,()=>{
+    console.log('Server is running on http://localhost:3000');
+});
+
+
+ 
